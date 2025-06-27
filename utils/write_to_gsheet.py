fe89908071871e94e832afe286394a634ed94581
adithya_724 <adithyamadhu2@gmail.com>
2025-06-27 12:25:51 +0530

    feat: modified prompt to confine values and added date parser for date

--- utils/write_to_gsheet.py
+++ utils/write_to_gsheet.py
@@ -45,6 +45,10 @@ def add_values_to_sheet(sample_values):
     worksheet = sheet.worksheet("Tracker")
 
     values_to_add = list(sample_values.values())
+    try:
+        values_to_add[0] = datetime.strptime(values_to_add[0], "%m/%d/%Y")
+    except Exception:
+        pass
     values_to_add = [values_to_add[0], values_to_add[1], "", "", *values_to_add[2:]]
 
     start_row = get_next_row()
